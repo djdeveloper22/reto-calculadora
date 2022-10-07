ede7eb697e960c471dbf7a56dbb52d602e24da35
djdeveloper22 <djdeveloper@gmail.com>
2022-10-07 16:09:45 -0500

    se quitan espacios entre el codigo

--- javascript/reto-4/reto-4.js
+++ javascript/reto-4/reto-4.js
@@ -8,25 +8,19 @@ if(isNaN(notaFinal)){
   console.log('no es un numero el dato ingresado');
 }else{
   if(notaFinal >=0 && notaFinal <3){
-    console.log(`su nota final es: ${notaFinal}.... Muy deficiente`)
-    
+    console.log(`su nota final es: ${notaFinal}.... Muy deficiente`);
   }else if(notaFinal && 3 && notaFinal < 5){
-    console.log(`su nota final es: ${notaFinal}.... Insuficiente`)
-    
+    console.log(`su nota final es: ${notaFinal}.... Insuficiente`);
   }else if(notaFinal >= 5 && notaFinal < 6){
-    console.log(`su nota final es: ${notaFinal}.... Suficiente`)
-    
+    console.log(`su nota final es: ${notaFinal}.... Suficiente`);
   }else if(notaFinal >=6 && notaFinal < 7){
-    console.log(`su nota final es: ${notaFinal}.... Bien`)
-    
+    console.log(`su nota final es: ${notaFinal}.... Bien`);
   }else if(notaFinal >= 7 && notaFinal < 9){
-    console.log(`su nota final es: ${notaFinal}.... Notable`)
-    
+    console.log(`su nota final es: ${notaFinal}.... Notable`);
   }else if(notaFinal >= 9 && notaFinal <= 10){
-    console.log(`su nota final es: ${notaFinal}.... Sobresaliente`)
-
+    console.log(`su nota final es: ${notaFinal}.... Sobresaliente`);
   }else{
-    console.log('el numero ingresado esta fuera del rango notarial...')
+    console.log('el numero ingresado esta fuera del rango notarial...');
   }
 }
 console.groupEnd();
