501e25ce460ddff84e2981460318345a5807383f
djdeveloper22 <djdeveloper@gmail.com>
2022-10-21 10:52:03 -0500

    mejorando el reto POO blender

--- javascript/POO/reto-poo-1/index.js
+++ javascript/POO/reto-poo-1/index.js
@@ -3,7 +3,6 @@ const scanf = require("scanf");
 const { aMixer } = require("./src/core/BD");
 const { Blender } = require("./src/core/Mixer");
 let random_number = Math.floor(Math.random() * 4);
-let statusOff = true;
 let ob1_mixer = new Blender(
   aMixer[random_number].brand,
   aMixer[random_number].statusOff,
@@ -17,24 +16,22 @@ console.log(`--------------------------------------\n
   ----------------------------------------
 `);
 
-console.log("turn on blender yes/no");
-let choice = scanf("%s").toLocaleLowerCase;
-if(choice == 'yes'){
-  console.log('entro')
-  statusOff = false;
-  aMixer[random_number].statusOff;
+console.log('On blender(Y)...');
+var status = scanf('%S');
+if(status !== 'Y' && status !== 'N'){
+  console.log('Error');
+}else{
+  let cont = 0;
+  while(status !== 'Y')
+  cont++;
   console.log(`--------------------------------------\n
-    brand blender: ${ob1_mixer.brand} \n
-    statusOff: ${ob1_mixer.on(aMixer[random_number].statusOff)} \n
-    speed level: ${ob1_mixer.speed}\n
-    Current Speed: ${ob1_mixer.speedCurrent(Math.floor(Math.random()*aMixer[random_number].speed)+1)} \n
-    ----------------------------------------
+  brand blender: ${ob1_mixer.brand} \n
+  statusOff: ${ob1_mixer.on(aMixer[random_number].statusOff)} \n
+  speed level: ${ob1_mixer.speed}\n
+  ----------------------------------------
   `);
-}else if(choice == 'no'){
-  console.log('Blender Off');
+  console.log('apagar(N)');
+  status = scanf('%S');
 }
-//console.clear();
-
-
-
+console.log('off');
 
