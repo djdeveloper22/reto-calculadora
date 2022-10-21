b768cb646cd213e2c450735b82be59f56942997d
djdeveloper22 <djdeveloper@gmail.com>
2022-10-21 11:02:33 -0500

    se utiliza el metodo de velocidad actual

--- javascript/POO/reto-poo-1/index.js
+++ javascript/POO/reto-poo-1/index.js
@@ -1,4 +1,3 @@
-
 const scanf = require("scanf");
 const { aMixer } = require("./src/core/BD");
 const { Blender } = require("./src/core/Mixer");
@@ -6,7 +5,7 @@ let random_number = Math.floor(Math.random() * 4);
 let ob1_mixer = new Blender(
   aMixer[random_number].brand,
   aMixer[random_number].statusOff,
-  aMixer[random_number].speed,
+  aMixer[random_number].speed
 );
 
 console.log(`--------------------------------------\n
@@ -16,23 +15,25 @@ console.log(`--------------------------------------\n
   ----------------------------------------
 `);
 
-console.log('On blender(Y)...');
-var status = scanf('%S');
-if(status !== 'Y' && status !== 'N'){
-  console.log('Error');
-}else{
+console.log("On blender(Y)...");
+var status = scanf("%S");
+if (status !== "Y" && status !== "N") {
+  console.log("Error");
+} else {
   let cont = 0;
   aMixer[random_number].statusOff = false;
-  while(status !== 'Y')
-  cont++;
+  while (status !== "Y") cont++;
   console.log(`--------------------------------------\n
   brand blender: ${ob1_mixer.brand} \n
   statusOff: ${ob1_mixer.on(aMixer[random_number].statusOff)} \n
   speed level: ${ob1_mixer.speed}\n
+  Current speed: ${ob1_mixer.speedCurrent(
+    Math.floor(Math.random() * aMixer[random_number].speed) + 1
+  )}
   ----------------------------------------
   `);
-  console.log('apagar(N)');
-  status = scanf('%S');
+  console.log("apagar(N)");
+  status = scanf("%S");
 }
-console.log('off');
-
+console.clear();
+console.log("off blender");
